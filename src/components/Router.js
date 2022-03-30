0878595b2e06430c240056e05381bc21066fd64a
JB <cool508@naver.com>
2022-03-30 21:41:03 +0900

    Create Router.js

---
+++ src/components/Router.js
@@ -0,0 +1,13 @@
+import { HashRouter as Router, Route, Swich } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Swich>
+        <Route />
+      </Swich>
+    </Router>
+  )
+}
+
+export default AppRouter
