3064ab80418441a47aa29cb1ef876ddbd51616c9
JB <cool508@naver.com>
2022-03-30 20:48:00 +0900

    Edit firebase.js Config

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDGgsAh4U0aGx_mYFLFpe9WX2WnMmEHGFc",
-  authDomain: "nwitter-d4947.firebaseapp.com",
-  projectId: "nwitter-d4947",
-  storageBucket: "nwitter-d4947.appspot.com",
-  messagingSenderId: "243570832747",
-  appId: "1:243570832747:web:a6c206b53f774b1a6b082e"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
