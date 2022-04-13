425513544d1f2e4dd3d5b40c8cd83c9748a58d21
JB <cool508@naver.com>
2022-04-13 21:35:12 +0900

    Edit Auth.js

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,42 @@
-const Auth = () => <span>Auth</span>
+import { useState } from "react";
 
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+
+    const onChange = (event) => {
+        const {
+            target: {name, value},
+        } = event;
+        if ( name === "email") {
+            setEmail(value);
+        } else if ( name === "password") {
+            setPassword(value);
+        }
+    }
+    
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (newAccount) {
+            // create newAccount
+        } else {
+            // Log in
+        }
+    }
+
+    return (
+        <div>
+            <form>
+                <input type="email" name="email" placeholder="Email" required  value={ email } onChange={ onChange } />
+                <input type="password" placeholder="Password" required value={ password } onChange={ onChange } />
+                <input type="submit" value={ newAccount ? "Create Account" : "Log In" }  />
+            </form>
+            <div>
+                <button>Continue with Goolge</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
 export default Auth
